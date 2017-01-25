1ded421483f44dd97f16704633c3bd2c0dea0062
Bill Barnes <bill@billba-mbp.local>
2017-01-25 09:52:10 -0800

    Update to use botchat from CDN

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 "use strict";
 require('dotenv').config();
-const express = require('express');
-const ejs_1 = require('ejs');
-const fetch = require('node-fetch');
-const fs_1 = require('fs');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
+const express = require("express");
+const ejs_1 = require("ejs");
+const fetch = require("node-fetch");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const app = express();
 app.use(cookieParser());
 app.use(bodyParser.json()); // for parsing application/json
@@ -14,29 +13,12 @@ const guid = () => {
     const s4 = () => Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 };
-app.get('/botchat', (req, res) => {
-    fs_1.readFile('./botchat/index.html', (err, data) => {
-        const contents = data.toString();
-        let settings = "";
-        settings += `, user: { id: "${guid()}", name: "your name here" }`;
-        if (req.cookies.settings) {
-            console.log("cookie", req.cookies.settings);
-            settings += `, formatOptions: { showHeader: "${req.cookies.settings.showHeader}" }`;
-            if (req.cookies.settings.allowMessagesFrom)
-                settings += `, allowMessagesFrom: ['${req.cookies.settings.allowMessagesFrom.replace(" ", "").replace(",", "','")}']`;
-        }
-        console.log("settings", settings);
-        res.send(contents.replace("// ADD MORE CONFIG HERE", settings));
-    });
-});
-app.use('/botchat', express.static('botchat'));
 app.post('/', (req, res) => {
     res.cookie('settings', req.body);
     res.redirect('/');
 });
 app.get('/', (req, res) => {
     const appSecret = (req.cookies.settings && req.cookies.settings.secret) || process.env.APP_SECRET;
-    console.log("query", req.query);
     const endpoint = 'https://directline.botframework.com/v3/directline/tokens/generate';
     const auth = 'Bearer';
     fetch(endpoint, {
@@ -45,11 +27,9 @@ app.get('/', (req, res) => {
     }).then(response => response.json()).then(result => {
         const token = result["token"];
         console.log("token", token, "retrieved at", new Date());
-        ejs_1.renderFile("./index.html", {
+        ejs_1.renderFile("./index.ejs", {
             token,
-            showHeader: req.cookies.settings && req.cookies.settings.showHeader,
-            secret: req.cookies.settings && req.cookies.settings.secret,
-            allowMessagesFrom: req.cookies.settings && req.cookies.settings.allowMessagesFrom
+            secret: req.cookies.settings && req.cookies.settings.secret
         }, (err, str) => {
             if (err)
                 console.log("ejs error", err);
--- index.ts
+++ index.ts
@@ -11,8 +11,7 @@ const app = express();
 app.use(cookieParser())
 
 interface Settings {
-    appSecret: string,
-    showHeader: string
+    appSecret: string
 }
 
 app.use(bodyParser.json()); // for parsing application/json
@@ -23,24 +22,6 @@ const guid = () => {
     return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4();
 }
 
-app.get('/botchat', (req, res) => {
-    readFile('./botchat/index.html', (err, data) => {
-        const contents = data.toString();
-        let settings = "";
-        settings += `, user: { id: "${guid()}", name: "your name here" }`;
-        if (req.cookies.settings) {
-            console.log("cookie", req.cookies.settings);
-            settings += `, formatOptions: { showHeader: "${req.cookies.settings.showHeader}" }`;
-            if (req.cookies.settings.allowMessagesFrom)
-                settings += `, allowMessagesFrom: ['${req.cookies.settings.allowMessagesFrom.replace(" ","").replace(",","','")}']`;
-        }
-        console.log("settings", settings);
-        res.send(contents.replace("// ADD MORE CONFIG HERE", settings));
-    })
-});
-
-app.use('/botchat', express.static('botchat'));
-
 app.post('/', (req, res) => {
     res.cookie('settings', req.body)
     res.redirect('/');
@@ -48,7 +29,6 @@ app.post('/', (req, res) => {
 
 app.get('/', (req, res) => {
     const appSecret = (req.cookies.settings && req.cookies.settings.secret) || process.env.APP_SECRET;
-    console.log("query", req.query);
     const endpoint = 'https://directline.botframework.com/v3/directline/tokens/generate';
     const auth = 'Bearer';
     fetch(endpoint, {
@@ -59,11 +39,9 @@ app.get('/', (req, res) => {
     ).then(result => {
         const token: string = result["token"];
         console.log("token", token, "retrieved at", new Date());
-        renderFile("./index.html", {
+        renderFile("./index.ejs", {
             token,
-            showHeader: req.cookies.settings && req.cookies.settings.showHeader, 
-            secret: req.cookies.settings && req.cookies.settings.secret,
-            allowMessagesFrom: req.cookies.settings && req.cookies.settings.allowMessagesFrom
+            secret: req.cookies.settings && req.cookies.settings.secret
         }, (err, str) => {
             if (err)
                 console.log("ejs error", err);
